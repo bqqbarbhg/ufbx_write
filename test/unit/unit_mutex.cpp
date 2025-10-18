eff1e3cba709ef098de03a9937aff9bb98de6ee2
bqqbarbhg <bqqbarbhg@gmail.com>
2025-10-19 02:35:07 +0300

    mutex_try_lock() test

--- test/unit/unit_mutex.cpp
+++ test/unit/unit_mutex.cpp
@@ -27,3 +27,28 @@ UFBXWT_UNIT_TEST(mutex_lock)
 
 	ufbxwt_assert(value == num_threads * num_iters);
 }
+
+UFBXWT_UNIT_TEST(mutex_try_lock)
+{
+	ufbxwt_thread_pool thread_pool;
+	ufbxwi_thread_pool &tp = thread_pool.tp;
+
+	uint32_t value = 0;
+	ufbxwi_mutex mutex = { };
+
+	const size_t num_threads = 16;
+	const size_t num_iters = 100000;
+	fork_threads(num_threads, num_iters, [&](size_t id, size_t index) {
+		for (;;) {
+			if (!ufbxwi_mutex_try_lock(&tp, &mutex)) {
+				continue;
+			}
+
+			value++;
+			ufbxwi_mutex_unlock(&tp, &mutex);
+			break;
+		}
+	});
+
+	ufbxwt_assert(value == num_threads * num_iters);
+}
