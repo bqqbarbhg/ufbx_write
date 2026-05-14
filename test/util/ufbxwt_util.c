77cf6a323b6b373101dd2417d00a1257d524bf89
bqqbarbhg <bqqbarbhg@gmail.com>
2026-05-14 19:40:02 +0300

    Zmij for tests

--- test/util/ufbxwt_util.c
+++ test/util/ufbxwt_util.c
@@ -16,6 +16,11 @@
 	#include "../../extra/ufbxw_to_chars.h"
 #endif
 
+#ifdef UFBXWT_HAS_ZMIJ
+	#include "../../extra/ufbxw_zmij.h"
+	#include "../../extra/ufbxw_zmij_c.h"
+#endif
+
 #ifdef UFBXWT_HAS_CPP_THREADS
 	#include "../../extra/ufbxw_cpp_threads.h"
 #endif
@@ -91,6 +96,20 @@ bool ufbxwt_ascii_format_setup(ufbxw_ascii_formatter *formatter, ufbxwt_ascii_fo
 		#endif
 		return false;
 
+	case UFBXWT_ASCII_FORMAT_IMPL_ZMIJ:
+		#if UFBXWT_HAS_ZMIJ
+			ufbxw_zmij_setup(formatter);
+			return true;
+		#endif
+		return false;
+
+	case UFBXWT_ASCII_FORMAT_IMPL_ZMIJ_C:
+		#if UFBXWT_HAS_ZMIJ
+			ufbxw_zmij_c_setup(formatter);
+			return true;
+		#endif
+		return false;
+
 	default:
 		ufbxwt_assert(false);
 		break;
@@ -104,8 +123,28 @@ const char *ufbxwt_ascii_format_name(ufbxwt_ascii_format_impl impl)
 	case UFBXWT_ASCII_FORMAT_IMPL_DEFAULT: return "default";
 	case UFBXWT_ASCII_FORMAT_IMPL_FMTLIB: return "fmtlib";
 	case UFBXWT_ASCII_FORMAT_IMPL_TO_CHARS: return "to_chars";
+	case UFBXWT_ASCII_FORMAT_IMPL_ZMIJ: return "zmij";
+	case UFBXWT_ASCII_FORMAT_IMPL_ZMIJ_C: return "zmij_c";
 	default: return "";
 	}
+}
+
+bool ufbxwt_ascii_format_supports_float_format(ufbxwt_ascii_format_impl impl, ufbxw_ascii_float_format format)
+{
+	switch (impl) {
+	case UFBXWT_ASCII_FORMAT_IMPL_DEFAULT: return true;
+	case UFBXWT_ASCII_FORMAT_IMPL_FMTLIB: return true;
+	case UFBXWT_ASCII_FORMAT_IMPL_TO_CHARS: return true;
+
+	case UFBXWT_ASCII_FORMAT_IMPL_ZMIJ:
+	case UFBXWT_ASCII_FORMAT_IMPL_ZMIJ_C:
+		return format == UFBXW_ASCII_FLOAT_FORMAT_ROUND_TRIP;
+
+	default:
+		ufbxwt_assert(false);
+		break;
+	}
+	return false;
 }
 
 bool ufbxwt_thread_setup(ufbxw_thread_sync *sync, ufbxw_thread_pool *pool, ufbxwt_thread_impl impl)
@@ -134,4 +173,3 @@ const char *ufbxwt_thread_impl_name(ufbxwt_thread_impl impl)
 	default: return "";
 	}
 }
-
